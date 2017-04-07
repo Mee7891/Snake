50b59fd213bf5a9dbd90812e4abbef4eab7915be
Mikhail Dubovik <m.dubovik@inbox.ru>
2017-04-07 23:04:28 +0500

    Создан класс PointEventArgs

---
+++ PointEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    public class PointEventArgs: System.EventArgs
+    {
+        public int X    { get; private set; }
+        public int Y    { get; private set; }
+        public char Sym { get; private set; }
+
+        public PointEventArgs(int x, int y, char sym)
+        {
+            X = x; Y = y; Sym = sym;
+        }
+    }
+}
