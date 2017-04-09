83bfe745f09f5afdb1420a5f37f183b5ae5e85ad
Mikhail Dubovik <m.dubovik@inbox.ru>
2017-04-09 05:59:49 +0500

    Создан абстрактный класс Figure

--- Figure.cs
+++ Figure.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Snake1
 {
-    class Figure
+    abstract class Figure
     {
         List<Point> pList;
 
+        public void Draw()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
 
+        public void setEventHandler(EventHandler<PointEventArgs> handler)
+        {
+            foreach(Point p in pList)
+            {
+                p.Drawn += handler;
+            }
+        }
     }
 }
