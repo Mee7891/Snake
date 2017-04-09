8d92faa631318c238aacfd14753a2a1c08949fe4
Mikhail Dubovik <m.dubovik@inbox.ru>
2017-04-09 07:01:06 +0500

    Добавлен ранее удаленный файл Figure.cs

---
+++ Figure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    abstract class Figure
+    {
+        protected List<Point> pList;
+
+        public void Draw()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+        public void setEventHandler(EventHandler<PointEventArgs> handler)
+        {
+            foreach(Point p in pList)
+            {
+                p.Drawn += handler;
+            }
+        }
+    }
+}
