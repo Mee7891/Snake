0e1a684771fa8a1f1d1f0b639009aa574258c43e
Mikhail Dubovik <m.dubovik@inbox.ru>
2017-04-09 06:12:39 +0500

    Создан класс HorisontalLine

---
+++ HorisontalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class HorisontalLine: Figure
+    {
+        public HorisontalLine(int xMin, int xMax, int y, char sym)
+        {
+            pList = new List<Point>();
+
+            for(int i = xMin; i<=xMax; i++)
+            {
+                pList[i] = new Point(i, y, sym);
+            }
+        }
+    }
+}
