b76b7b71767563473bc663d6afd60248823d854f
Mikhail Dubovik <m.dubovik@inbox.ru>
2017-04-17 22:18:07 +0500

    Добавлены перегруженные конструкторы в Snake

--- Snake.cs
+++ Snake.cs
@@ -10,14 +10,19 @@ namespace Snake1
     {
         Direction dir;
 
-        public Snake(int xTail, int yTail, int lenght, Direction _dir)
+        public Snake(int xTail, int yTail, int length, Direction _dir): 
+            this(new Point(xTail, yTail), length, _dir) {}
+
+        public Snake(int xTail, int yTail, char sym, int length, Direction _dir) :
+            this(new Point(xTail, yTail, sym), length, _dir) {}
+
+        public Snake(Point p, int length, Direction _dir)
         {
             dir = _dir;
             pList = new List<Point>();
 
-            for (int i = 0; i < lenght; i++)
+            for (int i = 0; i < length; i++)
             {
-                Point p = new Point(xTail, yTail);
                 p.Move(dir, i);
                 pList.Add(p);
             }
